014abdd06f1409cc55deea485942cd0c94b0cbc2
wuzehui <wuzehui@zuinianqing.com>
2019-08-13 18:50:31 +0800

    add languge toggle function

--- switch-app.lua
+++ switch-app.lua
@@ -5,54 +5,68 @@ local application = require 'hs.application'
 local hyper = {'cmd', 'alt', 'ctrl', 'shift'}
 
 local key2App = {
-    e = 'Emacs',
-    f = 'Finder',
-    g = 'Google Chrome',
-    v = 'Preview',
-    y = 'MailMaster',
-    p = 'PhpStorm',
-    w = 'WeChat',
-    i = 'iTerm',
-    d = 'Microsoft Word',
-    x = 'Microsoft Excel',
-    r = 'DataGrip',
-    q = 'QQ'
+   e = 'Emacs',
+   f = 'Finder',
+   g = 'Google Chrome',
+   -- v = 'Preview',
+   v = 'Adobe Acrobat Reader DC',
+   y = 'MailMaster',
+   p = 'PhpStorm',
+   w = 'WeChat',
+   i = 'iTerm',
+   d = 'Microsoft Word',
+   x = 'Numbers',
+   r = 'DataGrip',
+   q = 'QQ'
 }
 
 for key, app in pairs(key2App) do
-    hotkey.bind(hyper, key, function()
-        toggle_application(app)
-    end)
+   hotkey.bind(hyper, key, function()
+                  toggle_application(app)
+   end)
+end
+
+-- 添加语言切换函数
+local function toggle_languge(_app)
+   print(_app)
+   if (_app == 'PhpStorm' or _app == 'Emacs' or _app == 'DataGrip' or _app == 'iTerm') then
+      hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
+   else
+      hs.keycodes.currentSourceID("com.sogou.inputmethod.sogou.pinyin")
+   end
 end
 
 -- Toggle an application between being the frontmost app, and being hidden
 function toggle_application(_app)
-    -- finds a running applications
-    local app = application.find(_app)
+   -- finds a running applications
+   local app = application.find(_app)
 
-    if not app then
-        -- application not running, launch app
-        application.launchOrFocus(_app)
-        return
-    end
+   if not app then
+      -- application not running, launch app
+      application.launchOrFocus(_app)
+      toggle_languge(_app)
+      return
+   end
 
-    -- application running, toggle hide/unhide
-    local mainwin = app:mainWindow()
-    if mainwin then
-        if true == app:isFrontmost() then
-            mainwin:application():hide()
-        else
-            mainwin:application():activate(true)
-            mainwin:application():unhide()
-            mainwin:focus()
-        end
-    else
-        -- no windows, maybe hide
-        if true == app:hide() then
-            -- focus app
-            application.launchOrFocus(_app)
-        else
-            -- nothing to do
-        end
-    end
+   -- application running, toggle hide/unhide
+   local mainwin = app:mainWindow()
+   if mainwin then
+      if true == app:isFrontmost() then
+         mainwin:application():hide()
+      else
+         mainwin:application():activate(true)
+         mainwin:application():unhide()
+         mainwin:focus()
+      end
+   else
+      -- no windows, maybe hide
+      if true == app:hide() then
+         -- focus app
+         application.launchOrFocus(_app)
+      else
+         -- nothing to do
+      end
+   end
+
+   toggle_languge(_app)
 end
